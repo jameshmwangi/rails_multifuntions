75a41095fd4ac6c2bb579fac1c17644943e20333
James Mwangi K <jameshmwangiy@gmail.com>
2026-03-14 18:51:16 +0300

    update the welcome email subject to Japanese  -

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module RailsFunctionsTaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-      config.load_defaults 6.1
+    config.load_defaults 6.1
+    config.active_storage.track_variants = false
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
@@ -20,10 +21,6 @@ module RailsFunctionsTaskApp
         request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-      # config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :sidekiq
   end
 end
