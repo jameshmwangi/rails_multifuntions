6f299fb16165a563ed94a1fe3b893d9ad0d8392b
James Mwangi K <jameshmwangiy@gmail.com>
2026-03-16 21:32:41 +0300

    added fix to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,4 +18,24 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include FactoryBot::Syntax::Methods
+
+  # Disable transactional fixtures for system tests (Capybara needs persisted data)
+  config.before(:each, type: :system) do
+    config.use_transactional_fixtures = false
+  end
+
+  config.after(:each, type: :system) do
+    config.use_transactional_fixtures = true
+  end
+
+  # Cleanup database and ActionMailer between tests
+  config.before(:each, type: :system) do
+    # Clear ActionMailer deliveries to prevent test pollution
+    ActionMailer::Base.deliveries.clear
+  end
+
+  config.after(:each, type: :system) do
+    # Truncate all tables after system tests
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE users, async_logs, active_storage_attachments, active_storage_blobs, active_job_locks RESTART IDENTITY CASCADE")
+  end
 end
